c242d203b276d32956a2cd96d5aac2e64a60630d
BPBN <BPBN@ya.ru>
2024-03-18 16:53:39 +0300

    Переписал код "Крестики нолики"

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,70 +1,45 @@
-board = {1:'-', 2:'-', 3:'-', 4:'-', 5:'-', 6:'-', 7:'-', 8:'-', 9:'-'}
-
-wins_coord = [(1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7), (2, 5, 8), (3, 6, 9), (7, 5, 3), (1, 5, 9)]
+board = [[' '] * 3 for i in range(3)]
 
 print('\nДля выбора позиции выберите номер строки по\nвертикали и номер столбца по горизонтали.'
-          '\n\t\t\t(Пример: 00)')
+          '\n\t\t\t(Пример: 01)')
 
 
 def draw_board():
-    print('\n\t\t\t    0  1  2'
-          '\n\t\t\t 0  {b[1]}  {b[2]}  {b[3]}'
-          '\n\t\t\t 1  {b[4]}  {b[5]}  {b[6]}'
-          '\n\t\t\t 2  {b[7]}  {b[8]}  {b[9]}'.format(b=board))
+    print(f'\n\t\t\t  0   1   2\n\t\t\t-------------')
+    for i, row in enumerate(board):
+        row_str = f'\t\t {i}  | {" | ".join(row)} |'
+        print(row_str)
+        print(f'\t\t\t-------------')
 
 
 def take_inpud(player_token):
     while True:
-        num0 = 0
-        num1 = 1
-        num2 = 2
-
         value = input('\nКуда поставить: ' + player_token + ' ? ')
 
         if str(value) not in '001021011220212' and len(value) >= 3:
-            print('Ошибочный ввод. Повторите.')
+            print('Неверный ввод. Повторите.')
             continue
-        if value == str(num0) + str(num0):
-            value = 1
+        value1 = [int(i) for i in value]
 
-        if value == str(num0) + str(num1):
-            value = 2
-
-        if value == str(num0) + str(num2):
-            value = 3
-
-        if value == str(num1) + str(num0):
-            value = 4
-
-        if value == str(num1) + str(num1):
-            value = 5
-
-        if value == str(num1) + str(num2):
-            value = 6
-
-        if value == str(num2) + str(num0):
-            value = 7
-
-        if value == str(num2) + str(num1):
-            value = 8
-
-        if value == str(num2) + str(num2):
-            value = 9
-        if board[value] in 'XO':
+        if board[value1[0]][value1[1]] != ' ':
             print('Эта клетка уже занята.')
             continue
-        board[value] = player_token
+        board[value1[0]][value1[1]] = player_token
         break
 
-
-
 def check_win():
-    for each in wins_coord:
-        if board[each[0]] in 'X' and board[each[1]] in 'X' and board[each[2]] in 'X' or \
-                board[each[0]] in 'O' and board[each[1]] in 'O' and board[each[2]] in 'O':
-            return board[each[1]]
-    else:
-        return False
+    win_cord = (((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ((2, 0), (2, 1), (2, 2)),
+                ((0, 2), (1, 1), (2, 0)), ((0, 0), (1, 1), (2, 2)), ((0, 0), (1, 0), (2, 0)),
+                ((0, 1), (1, 1), (2, 1)), ((0, 2), (1, 2), (2, 2)))
+    for cord in win_cord:
+        symbols = []
+        for c in cord:
+            symbols.append(board[c[0]][c[1]])
+        if symbols == ["X", "X", "X"]:
+            return symbols[0]
+        if symbols == ["0", "0", "0"]:
+            return symbols[0]
+    return False
 
 
 def main():
